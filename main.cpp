d7e2072b01e981b331d4a59eb355250a17e271cd
David Pérez <hallaplay835@gmail.com>
2014-01-02 21:09:12 +0100

    Cumple la especificación v1 de la práctica. - Elimina el changelog, que se traslada a git log. - Añade ayuda.txt. - Modifica .gitignore para que no ignore cambios en ayuda.txt.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,14 @@
 /* --------------- AUTHORS ---------------
-- Iván Calvo (calvuxi@gmail.com).
 - David Pérez (hallaplay835@gmail.com).
+- Iván Calvo (calvuxi@gmail.com).
 */
 
-/* --------------- CHANGELOG ---------------
-v1.0 # 24/12/2013 (hallaplay835)
-	- Funcionalidad básica front-end.
-v2.0 # 27/12/2013 | (hallaplay835)
-	- Funcionalidad básica back-end.
-v3.0 # xx/xx/xxxx | ()
-v3.1 # xx/xx/xxxx | ()
-v3.11 # xx/xx/xxxx | ()
-v3.12 # xx/xx/xxxx | ()
-v3.13 # xx/xx/xxxx | ()
-v3.14 # xx/xx/xxxx | ()
-v3.141 # xx/xx/xxxx | ()
-v3.142 # xx/xx/xxxx | ()
-
-PI = 3.14159 26535 89793 23846 26433 83279 50288
-
+/*
 #### BUGS #### (~ indica resuletos)
 1. ~
 
 #### SUGGESTIONS #### (~ indica llevadas a cabo)
-1. ~
+1. Preguntar cuál es el propósito de que un archivo de texto del que se van a copiar strings acabe en centinela.
 */
 
 // #### Libraries ####
@@ -33,7 +18,7 @@ PI = 3.14159 26535 89793 23846 26433 83279 50288
 #include <ctime>
 #include <string>
 #include <fstream>
-// #include <iomanip>
+#include <iomanip>
 
 using namespace std;
 
@@ -41,14 +26,16 @@ typedef unsigned short int usi;
 
 // #### Constants ####
 const string INVALID_TYPE = "ERROR. Tipo de datos no válido.";
+const string HELP_FILE = "ayuda.txt";
 const string CENTINEL = "xxx";
+const string PROMPT = "Código (? para ayuda, 0 para cancelar): ";
 const usi CHIPS = 4, TRIES = 30, COLORS = 6;
 const bool REPTS = false; // false = sin repeticiones, true = con repeticiones.
 
 // #### other typedef declarations ####
 typedef enum tColores { Rojo, Azul, Verde, Negro, Granate, Marrón };
 typedef tColores tCodigo[CHIPS];
-typedef enum tChoice { good, cancel, help, error, repts };
+typedef enum tStatus { good, cancel, help, length_err, key_err, rept_err };
 
 // #### Prototypes ####
 void pause();
@@ -61,7 +48,12 @@ bool displayTXTFileWCentinel(string fileName, string centinel);
 void genRndKey(tCodigo key);
 void printKey(tCodigo key);
 char toColorId(tColores color);
-tChoice readCode(tCodigo code);
+tColores toColor(char id);
+bool correctKeys(string input);
+void totCodigo(string input, tCodigo code);
+tStatus readCode(tCodigo code);
+bool printPerformanceMsg(tCodigo code, tCodigo key, usi tries);
+void manageStatus(tStatus status, tCodigo code);
 
 // #### main() ####
 int main() {
@@ -70,22 +62,43 @@ int main() {
 	// Mostrar el menú y leer una opción:
 	usi opt = menu();
 	while (opt != 0) {
+		cout << endl;
 		switch (opt) {
 			case 1: // Jugar una partida a Mastermind.
-				/*tCodigo key;
-				while (true) {
-					genRndKey(key);
+				cout << "--- NUEVA PARTIDA ---" << endl << endl;
+				tCodigo key;
+				genRndKey(key);
+				// **************** <DEBUG> ****************
+				cout << "La clave es: ";
+				printKey(key);
+				cout << endl;
+				// **************** </DEBUG> ****************
+				tStatus status;
+				tCodigo code;
+				usi tries = 0;
+				bool won = false;
+				do {
+					status = readCode(code);
+					if (status == good) { // Se ha leído un código de tipo correcto.
+						tries++;
+						won = printPerformanceMsg(code, key, tries);
+						if (won) {
+							cout << "¡ENHORABUENA! Has ganado en " << tries << " intento" << (tries > 1 ? "s" : "") << "." << endl;
+						}
+					} else { // El código leído no era correcto, o se seleccionó la opción de ayuda o la de salir. Imprimir el mensaje que corresponda.
+						manageStatus(status, code);
+					}
+				} while (tries < TRIES && !won && status != cancel);
+				if (status != cancel && !won) { // Se salió del bucle porque se acabaron los intentos.
+					cout << "Se te han acabado los intentos. La clave era: ";
 					printKey(key);
-					pause();
-				}*/
-				break;
+					cout << "." << endl;
+				}
 		}
 		
 		// Mostrar el menú y leer otra opción:
 		opt = menu();
 	}
-	
-	// pause();
 
 	return 0;
 }
@@ -127,7 +140,7 @@ int readNat(string ERR_MSG, unsigned int n) {
 	return readInt(ERR_MSG, n);
 }
 
-/** Muestra el menú y devuelve un entero representando la selección del usuario. **/
+/** Muestra el menú y devuelve un usi representando la selección del usuario. **/
 usi menu() {
 	cout << endl;
 	cout << "--- MENÚ ---";
@@ -163,25 +176,21 @@ bool displayTXTFile(string fileName, unsigned int i, unsigned int j) {
 }
 
 /** Muestra en consola los contenidos del archivo de texto fileName.
- ** Si existen múltiples espacios entre string y string del archivo, se imprimirá solo uno.
- ** No se imprimen caracteres de control ni espacios al principio del archivo.
- ** Se dejarán de mostrar los contenidos del archivo en caso de leer el string centinel, independientemente
- ** de si se ha llegado al final del archivo.
- ** Devuelve true si el archivo se muestra con éxito, false en caso contrario. **/
+ ** Si se lee una línea acabada en CENTINEL, será la última línea que se imprimirá. **/
 bool displayTXTFileWCentinel(string fileName, string centinel) {
 	ifstream file;
 	file.open(fileName.c_str());
 	if (!file.is_open()) {
 		return false;
-	}
-	else {
+	} else {
 		// Imprimir archivo, copiando línea a línea:
 		string buff;
-		file >> buff;
-		while (!file.fail() && buff != centinel) {
-			cout << buff << " ";
-			file >> buff;
+		getline(file, buff);
+		while (!file.fail() && buff.substr(buff.length() - 3, 3) != centinel) {
+			cout << buff << endl;
+			getline(file, buff);
 		}
+		if (!file.fail() && buff != "centinel") cout << buff.substr(0, buff.length() - 3);
 		file.close();
 		return true;
 	}
@@ -216,10 +225,11 @@ void genRndKey(tCodigo key) {
 
 }
 
-/** Imprime los identificadores de key a consola. **/
+/** Imprime los identificadores de key a consola, seperados por un espacio. **/
 void printKey(tCodigo key) {
 	for (usi i = 0; i <= CHIPS - 1; i++) {
 		cout << toColorId(key[i]);
+		if (i != CHIPS - 1) cout << " ";
 	}
 }
 
@@ -249,21 +259,158 @@ char toColorId(tColores color) {
 	}
 }
 
-/* tChoice readCode(tCodigo code) {
-	cout << "Código (? para ayuda, 0 para cancelar): ";
+/** Devuelve el tColor del identificador id. En caso de no existir el identificador, devuelve Rojo. **/
+tColores toColor(char id) {
+	if (id == 'r' || id == 'R') {
+		return Rojo;
+	} else if (id == 'a' || id == 'A') {
+		return Azul;
+	} else if (id == 'v' || id == 'V') {
+		return Verde;
+	} else if (id == 'n' || id == 'N') {
+		return Negro;
+	} else if (id == 'g' || id == 'G') {
+		return Granate;
+	} else if (id == 'm' || id == 'M') {
+		return Marrón;
+	} else {
+		return Rojo;
+	}
+}
+
+/** Devuelve true si input contiene identificadores correctos, false en otro caso. Se asume que la longitud de input es CHIPS. **/
+bool correctKeys(string input) {
+	bool correctKeys = true;
+	for (usi i = 0; i <= CHIPS - 1 && correctKeys; i++) {
+		if (input[i] != 'r' && input[i] != 'a' && input[i] != 'v' && input[i] != 'n' && input[i] != 'g' && input[i] != 'm'
+		 && input[i] != 'R' && input[i] != 'A' && input[i] != 'V' && input[i] != 'N' && input[i] != 'G' && input[i] != 'M') {
+			correctKeys = false;
+		}
+	}
+	return correctKeys;
+}
+
+/** Copia en code los colores asociados a los ids de los chars de string. **/
+void totCodigo(string input, tCodigo code) {
+	for (usi i = 0; i <= CHIPS - 1; i++) {
+		code[i] = toColor(input[i]);
+	}
+}
+
+/** Pide y lee un código del usuario. Devuelve un estado y copia el código a code si es del tipo correcto. **/
+tStatus readCode(tCodigo code) {
+	cout << PROMPT;
 	string input;
 	getline(cin, input);
 	if (input == "0") {
 		return cancel;
 	} else if (input == "?") {
 		return help;
-	} else if (input.length() != CHIPS) {
-		return error;
-	} else {
-		// LLenar code con el código.
-		for (usi i = 0; i <= CHIPS - 1; i++) {
-			// if (input[i] != 'R' || input[i] != 'A' || input )
+	} else if (input.length() != CHIPS) { // Longitud del código inadecuada.
+		return length_err;
+	} else if (!correctKeys(input)) { // Longitud del código adecuada, identificadores incorrectos.
+		return key_err;
+	} else if (!REPTS) { // Longitud del código adecuada, identificadores correctos. Comprobar repeticiones si procede.
+		bool chart[COLORS];
+		bool repts = false; // Asumir que no hay repeticiones.
+
+		// Inicializar todas las entradas de chart a false.
+		for (usi i = 0; i <= COLORS - 1; i++) {
+			chart[i] = false;
 		}
+
+		for (usi i = 0; i <= CHIPS - 1 && !repts; i++) {
+			if (chart[toColor(input[i])]) { // Se había leído el identificador antes.
+				repts = true;
+			} else {
+				chart[toColor(input[i])] = true;
+			}
+		}
+
+		if (repts) {
+			return rept_err;
+		} else {
+			totCodigo(input, code);
+			return good;
+		}
+	} else { // Longitud del código adecuada, identificadores correctos. No hace falta comprobar repeticiones.
+		totCodigo(input, code);
 		return good;
 	}
-} */
+}
+
+/** Compara los contenidos de code y key, y muestra un mensaje en la consola indicando el número de aciertos totales
+ ** y parciales (colores del codigo que están en la clave, pero descolocados), así como el número de intentos.
+ ** Devuelve true si el jugador ha acertado por completo la clave, false en caso contrario. **/
+bool printPerformanceMsg(tCodigo code, tCodigo key, usi tries) {
+	usi correct_keys = 0, disordered_keys = 0;
+
+	bool chart[CHIPS];
+
+	// Inicializar todas las entradas de chart a false.
+	for (usi i = 0; i <= CHIPS - 1; i++) {
+		chart[i] = false;
+	}
+
+	// Calcular aciertos totales:
+	for (usi i = 0; i <= CHIPS - 1; i++) {
+		if (code[i] == key[i]) {
+			chart[i] = true;
+			correct_keys++;
+		}
+	}
+
+	// Calcular aciertos parciales:
+	bool found;
+	for (usi i = 0; i <= CHIPS - 1; i++) {
+		found = false;
+		for (usi j = 0; j <= CHIPS - 1 && !found; j++) {
+			if (!chart[j] && code[i] == key[j]) {
+				disordered_keys++;
+				chart[j] = true;
+				found = true;
+			}
+		}
+	}
+
+	cout << "   " << tries << ": ";
+	printKey(code);
+	cout << setfill(' ') << setw(8) << correct_keys << " ¬" << setw(6) << disordered_keys << " ~" << endl;
+
+	// Return:
+	if (correct_keys == CHIPS) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/** Imprime el mensaje correspondiente al estado status. **/
+void manageStatus(tStatus status, tCodigo code) {
+	switch (status) {
+		case cancel:
+			cout << "Has abandonado el juego.";
+			cout << endl;
+			break;
+		case help:
+			cout << endl << "--- AYUDA ---" << endl << endl;
+			if (!displayTXTFileWCentinel(HELP_FILE, CENTINEL)) {
+				cout << "No se ha encontrado el archivo de ayuda.";
+			}
+			cout << endl << endl;
+			break;
+		case length_err:
+			cout << "La longitud de los códigos debe ser de " << CHIPS << " caracter";
+			cout << (CHIPS > 1 ? "es" : "") << ".";
+			cout << endl;
+			break;
+		case key_err:
+			cout << "Los códigos sólo pueden tener ciertos colores (? para consultar los permitidos).";
+			cout << endl;
+			break;
+		case rept_err:
+			cout << "El modo de juego actual no admite códigos con colores repetidos.";
+			cout << endl;
+			break;
+	}
+}
